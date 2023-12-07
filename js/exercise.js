09dbc2bf12b1226f4fe3771185db7816c914d3c7
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-07 22:52:53 +0500

    jalol

---
+++ js/exercise.js
@@ -0,0 +1,73 @@
+// Task 1
+
+// let a = +prompt("To`g`ri to`rtburchak uzunligi: ");
+// let b = +prompt("To`g`ri to`rtburchak eni")
+
+// let S = b * a;
+// let P = 2 * (a + b)
+// console.log("Perimetr =", P);
+// console.log(" Yuzasi=", S);
+
+
+// Task 2
+
+// let D = +prompt("Doirasi diametri:");
+
+// const PI = 3.14;
+
+// L =  PI * D;
+// R = D / 2
+// S = PI * R ** 2;
+
+// console.log ("Uzunligi =", L)
+// console.log ("Yuzasi =", S)
+
+
+// Task 3
+
+// let  A= +prompt("To'rburchakni tomoni");
+
+//  V = A ** 3;
+//  S = 6 * A ** 2 
+
+// console.log("Hajim =", V);
+// console.log(" To`la sirti: =", S);
+
+
+// Task 4 
+
+// let  A= +prompt("To'rburchakni tomoni");
+
+//  V = A * B * C;
+//  S =  2 * (a * b + b * c + a * c); 
+
+// console.log("Hajim =", V);
+// console.log(" To`la sirti: =", S);
+
+
+// Task 5
+
+// let  a= +prompt("Birinchi son");
+// let  b= +prompt("Birinchi son");
+
+//  M = (a + b) / 2;
+
+
+// console.log("O'rta arifmetigi =", M);
+
+
+//Task 6 
+
+
+// let R1= +prompt("Doirasi radiusi_1:");
+// let R2= +prompt("Doirasi radiusi_2:");
+
+// const PI = 3.14;
+
+// S1 = PI * R1;
+// S2 = PI * R2;
+// S = PI * (R1**2 - R2**2);
+
+// console.log("Yuza_1 =", S1)
+// console.log("Yuza_2 =", S2)
+// console.log("Yuza_umumiy =", S)
---
+++ js/exercises-1.js
@@ -0,0 +1,102 @@
+// let a = +prompt("To`g`ri to`rtburchak uzunligi: ");
+// let b = +prompt("To`g`ri to`rtburchak eni")
+
+// let S = b * a;
+// let P = 2 * (a + b)
+// console.log("Perimetr =", P);
+// console.log(" Yuzasi=", S);
+
+
+// Begin2
+
+
+
+// console.log("Uzunligi =", L);
+// console.log("Yuzasi =", S);
+
+// Begin4
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+
+// let G = (a * b) ** (1 / 2);
+
+// console.log(G);
+
+// Begin5
+
+// let x = +prompt("x ni kiriting");
+
+// let y = 3 * x ** 6 - 6 * x ** 2 - 7;
+
+// console.log(y);
+
+// Begin6
+
+// let X = +prompt("X kg");
+// let A = +prompt("A sum");
+// let Y = +prompt("Y kg");
+
+// let kg1 = A / X;
+
+// let kgY = Y * kg1;
+
+// console.log("1kg =", kg1);
+
+// console.log(Y, "kg = ", kgY);
+
+// Begin7
+
+// let A = 7;
+// let B = 10;
+
+// let k = A;
+
+// A = B;
+
+// B = k;
+
+// console.log(A);
+// console.log(B);
+
+// Integer1, 2, 3,
+
+// let ab = 62;
+
+// let b = ab % 10;
+
+// let a = (ab - b) / 10;
+
+// console.log(b);
+
+// console.log(a);
+
+// let ba = b * 10 + a;
+
+// console.log(ba);
+
+// Integer 4
+
+// let abc = 274;
+
+// bc = abc % 100;
+
+// a = (abc - bc) / 100;
+
+// console.log(a);
+
+// Integer6
+
+// let N = 10000;
+
+// let restSeconds = N % 3600;
+
+// let hour = (N - restSeconds) / 3600;
+
+// let second = restSeconds % 60;
+
+// let minute = (restSeconds - second) / 60;
+
+// console.log(hour);
+// console.log(minute);
+// console.log(second);
